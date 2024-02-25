2ca924cebd338248ff0865b012ef23dfa7131eb3
nlightnlabs <@nlightnlabs.com>
2024-02-25 14:47:48 -0800

    commit

--- src/MarketPlace.js
+++ src/MarketPlace.js
@@ -2,14 +2,18 @@ import React, {useState, useEffect} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import * as freeAgentApi from "./apis/freeAgent.js"
 import * as nlightnApi from './apis/nlightn.js';
-import {toProperCase} from "./functions/formatValue.js";
-
-import {AgGridReact} from 'ag-grid-react';
-import 'ag-grid-community/styles//ag-grid.css';
-import 'ag-grid-community/styles//ag-theme-quartz.css';
+import Cart from "./components/Cart.js"
+import Filter from "./components/Filter.js"
+import CatalogItem from "./components/CatalogItem.js"
+import OrderForm from "./components/OrderForm.js"
+import FloatingPanel from "./components/FloatingPanel.js"
 
 
 function MarketPlace() {
+
+  const [appData, setAppData] = useState({
+    icons: []
+})
 
     let environment = "freeagent"
     if(process.env.NODE_ENV ==="development"){
@@ -109,6 +113,26 @@ function MarketPlace() {
             getApps(appname)
        },500) 
     },[])
+
+    const getIcons = async()=>{
+      let appName = ""
+      if(environment ==="freeagent"){
+        appName = "icon"
+      }else{
+        appName = "icons" 
+      }
+      const data = await getData(appName)
+    
+      setAppData(prevAppData => ({
+        ...prevAppData,
+        icons: data
+      }));
+    };
+
+
+    useEffect(()=>{
+      getIcons();
+    })
 
 
     const handleGetData = async ()=>{
@@ -235,62 +259,7 @@ function MarketPlace() {
   return (
     <div className="d-flex flex-column" style={pageStyle}>
 
-        <h2 className="text-center">nlightnlabs FreeAgent Iframe Test</h2>
-
-        <div className="d-flex w-100" style={{height:"90%", width: "100%"}}>
-
-        <div className="d-flex flex-column m-3 bg-light p-3 rounded-3 shadow" style={{position: "relative", width: "300px", height:"100%", overflowY: "hidden"}}>
-
-            <div className="form-floating mb-3">
-                <select name= "app_name" value={appLabel} placeholder="app_name" onChange={(e)=>handleSelectApp(e)} 
-                    className="form-control" 
-                    style={{ fontSize: "12px", color: "rgb(50,150,250)"}}
-                    >
-                    {appList.map((item,index)=>(
-                        <option key={index} value={item}>{item}</option>
-                    ))}
-                </select>
-                <label htmlFor="app_name" className="form-label">App system name: </label>
-            </div>
         
-
-            <div className="d-flex justify-content-center mb-3">
-                <button className="btn btn-primary" onClick={(e)=>handleGetData()}>Get Data</button>
-            </div>
-
-            {appName !="" && appName !=null && data.length>0 &&
-                <div className="d-flex flex-column" style={{borderTop: "1px solid lightgray", height:"100%", overflowY: "hidden"}}>
-                    {Object.keys(formData).length> 0  && 
-                        <div className="d-flex flex-column p-1" style={{height:"80%", overflowY: "auto"}}>
-                            {Object.keys(formData).map((key, index)=>(
-                                <div key={index} className="form-floating mb-3">
-                                    <input id={key} name= {key} value={formData[key] || ""} className="form-control" placeholder={key} onChange={(e)=>handleInputChange(e)} style={{fontSize: "12px", color: "rgb(50,150,250)"}}></input>
-                                    <label htmlFor={key} className="form-label">{toProperCase(key.replaceAll("_"," "))}</label>
-                                </div>
-                            ))}
-                        </div>
-                    }
-                    <div className="d-flex justify-content-center mt-3">
-                        <div className="btn-group">
-                            <button className="btn btn-success" onClick={(e)=>addRecord()}>Add</button>
-                            <button className="btn btn-warning" onClick={(e)=>updateRecord()}>Update</button>
-                            <button className="btn btn-danger" onClick={(e)=>deleteRecord()}>Delete</button>
-                        </div>
-                    </div>
-                </div> 
-            }
-        </div> 
-
-        <div className="d-flex p-3 flex-column w-75" style={{height:"100%"}}>
-            <div id="myGrid" style={{height: "100%", width:"100%"}} className="ag-theme-quartz">
-                <AgGridReact
-                    rowData={data}
-                    columnDefs={fields}
-                    onCellClicked = {onCellClicked}
-                />
-            </div>
-        </div>
-        </div>
     </div>
   );
 }
